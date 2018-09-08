1c1200b1eca209600591587caf121f28c3fe84a1
Trubasa <1141521502@qq.com>
2018-09-08 09:07:42 +0800

    1

--- server.js
+++ server.js
@@ -3,28 +3,37 @@
  */
 const path=require('path')
 const express=require('express');
+const bodyParser=require('body-parser');
 const serverConfig=require(path.resolve('./config/serverConfig.js'));
 let api=require(path.resolve('./api/index.js'));
 let app=express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/userInfo',function (req,res) {
     var end=api.response();
-    end.data=[{use:'maik',age:'18'}]
-    setTimeout(()=>{
-        res.send(JSON.stringify(end))
-    },2000)
-})
-app.get('/userList',function (req,res) {
-    var end=api.response();
-    end.data=[{use:'maik',age:'18'}]
+    end.data={use:'maik',age:'18'}
     setTimeout(()=>{
         res.send(JSON.stringify(end))
     },1000)
 })
+app.get('/userList',function (req,res) {
+    var end=api.response();
+    end.data={use:'maik',age:'18'}
+    setTimeout(()=>{
+        res.send(JSON.stringify(end))
+    },500)
+})
 
 app.post('/login',function (req,res) {
+
     var end=api.response();
-    end.msg="登陆成功"
+    end.msg="登陆成功";
+    end.data=req.body
     res.send(JSON.stringify(end));
 })
 
